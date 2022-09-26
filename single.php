1d42210ca24509e9273d85fd84e94c51cd202e53
yurippe7575 <kkyyssmm75@gmail.com>
2022-09-26 09:17:33 +0900

    修正

--- single.php
+++ single.php
@@ -18,10 +18,8 @@
         <div class="article-inner">
           <h2 class="article-title"><?php the_title(); ?></h2>
           <?php the_content(); ?>
+          
           <div class="article-body">
-            <!-- <h3 class="article-info-title">URL</h3>
-            <p><a href="" target="_blank" rel="noopener"></a>
-              <br>pcのみ</p> -->
               
 
             <h3 class="article-info-title">制作期間</h3>
@@ -32,25 +30,16 @@
             </p>
             <h3 class="article-info-title">作業範囲</h3>
             <p> <?php echo $cfs->get('work_range'); ?></p>
-           
-            <?php $works_concept = $field['work_concept'];
-               if($works_concept) :?>
-                <h3 class="article-info-title">作品コンセプト・ポイントなど</h3>
-            <p>
-            <?php echo $cfs->get('work_concept'); ?>
+
+            <h3 class="article-info-title">URL</h3>
+            <p> <?php echo $cfs->get('work_url'); ?></p>
+
+         <!--  <h3 class="article-info-title">作品コンセプト・ポイントなど</h3>
+              <p>
+            
             </p>
-             <?php else:?>
-             <?php endif;?>
-
-            <?php $works_url = $field['work_url'];
-               if($works_url) :?>
-
-                <h3 class="article-info-title">URL</h3>
-            <p>
-            <?php echo $field['work_url']; ?>
-               </p>
-                      <?php else:?>
-                        <?php endif;?>
+ -->
+    </div>
           </div>
           <div class="home-link">
             <a href="index.html#works">Works一覧へ</a>
